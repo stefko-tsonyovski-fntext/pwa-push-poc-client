394e79953f4c7324a64add00b339183c5a6bc7d8
stefko-tsonyovski-fntext <stefko.tsonyovski.ext@fntsoftware.com>
2024-02-21 18:34:35 +0200

    Commit

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -142,10 +142,7 @@ function registerValidSW(swUrl, config) {
     })
     .catch((error) => {
       console.error("Error during service worker registration:", error);
-      toast.error(
-        "Error during service workers registration: " +
-          Object.keys(error).join(", ")
-      );
+      toast.error(error?.stack);
     });
 }
 
