0da460c9d1a49c1c4134bbae368d8c037fb88b3e
stefko-tsonyovski-fntext <stefko.tsonyovski.ext@fntsoftware.com>
2024-02-21 15:34:58 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   const [showSubscribe, setShowSubscribe] = useState(true);
   const [error, setError] = useState("");
   const [accessToken, setAccessToken] = useState(
-    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfWDlqTkF2bU5WVUNUWVVaNlBTQWZfX21UdDdQcEJHWk85Z1pCT1ZDc1pNIn0.eyJleHAiOjE3MDg1MjIzMDEsImlhdCI6MTcwODUyMjAwMSwianRpIjoiZmU0YWE3ZWUtMDAyYi00YzYwLThmMTAtZTg4MGI0MDNkMTU3IiwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50LmRldi5lLWZhY3QuYXBwL3JlYWxtcy9waWNhcmQiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiMTM4NzNlMTktYjYyYi00ZTQ5LTg1NDQtY2FkM2ZlMjU5MzczIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoid2ViLWFwcCIsInNlc3Npb25fc3RhdGUiOiJmMWMyOWI3Zi02YmI1LTRhMDQtYTI3MC0wODZhZDJmZTkzODciLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vYXBwc3J2LXdldS1mbnQtZGV2LWZlLmF6dXJld2Vic2l0ZXMubmV0IiwiKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1waWNhcmQiLCJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZSIsInNpZCI6ImYxYzI5YjdmLTZiYjUtNGEwNC1hMjcwLTA4NmFkMmZlOTM4NyIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhc3NldHMzQGFidi5iZyIsImVtYWlsIjoiYXNzZXRzM0BhYnYuYmcifQ.aG8jNfTeGxY64VQUXZkaPKgqcLpsSYesFN4-mEGzwIUrVfM5AlSLtr9fXSr0lf7IvO0l0XbqRNTJRCmh6uI494WPIRTSIuoichyXCDezxtYKhvmsZh3oXq2M-lG0prd3ddjeQCcnYZtUtHwb2VGp1ombdoljVLw5DxNn37uOYZqbYe9nT9XfjxfIAv872s9KoJu4o7a3cI-CoWmLkZTXoM5bXXcjq0IXs3608IiHRN1qsltwyYoC43XEp6fb9sUyGeqbZ_HIwscmxnORbuNqlGIKvmWTzEKBsi46MoVyd5s5yVpAaMllwCTEuizStuu0qBqroumD15G9ziWG2Sc8dg"
+    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfWDlqTkF2bU5WVUNUWVVaNlBTQWZfX21UdDdQcEJHWk85Z1pCT1ZDc1pNIn0.eyJleHAiOjE3MDg1MjI3NzUsImlhdCI6MTcwODUyMjQ3NSwianRpIjoiYTY1ZjU1N2UtNGQzMS00OGUxLTgzMzEtYTUyNTk3Nzc2YmNjIiwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50LmRldi5lLWZhY3QuYXBwL3JlYWxtcy9waWNhcmQiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiMTM4NzNlMTktYjYyYi00ZTQ5LTg1NDQtY2FkM2ZlMjU5MzczIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoid2ViLWFwcCIsInNlc3Npb25fc3RhdGUiOiIxZjhkMjYyMC1hZDRhLTQ3NGItOTk0Ni05NjlmOTYzYzUxY2MiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vYXBwc3J2LXdldS1mbnQtZGV2LWZlLmF6dXJld2Vic2l0ZXMubmV0IiwiKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1waWNhcmQiLCJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZSIsInNpZCI6IjFmOGQyNjIwLWFkNGEtNDc0Yi05OTQ2LTk2OWY5NjNjNTFjYyIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhc3NldHMzQGFidi5iZyIsImVtYWlsIjoiYXNzZXRzM0BhYnYuYmcifQ.qGjT8VhEkfkJEY9t8sOUy7GQHgvjukLeL3RXG6QLg00vR6KMSOLenT98miJO5qXYLloJi0uXbxNDYG1osOcisdjdbmVY6FmTXyLbHCLzKAWl1MSBzqtPEcw-NHWs3AGjMCnEtZuNG68EB10yqQqPFq66fRgJkIn6FZE9-aCz7O7NbElB98aQHIIulSlaXnXXQZJlVX23pYdhYpDPeMNe0ZtIJlXF0Rl891pVUusM_648Y9tZIwpsnG5D40TOb_KnoGew8610C-VC4dUVgh-vaFmzwckqJ0oEEJzkT7nmfIrY_WwK4aEkpFhDLecvjwRUhQ9DTBM47zUlwvXfSuFDng"
   );
 
   const onShowSubscribe = () => {
@@ -52,6 +52,61 @@ function App() {
   const onSubmitSubscribe = async (e) => {
     e.preventDefault();
     setLoadingSubscribe(true);
+    try {
+      const subscription = await getSubscription();
+      console.log(subscription.toJSON());
+
+      await axios.post(
+        BACKEND_URL + "/users/notifications/subscribe",
+        {
+          endpoint: subscription.endpoint,
+          p256dh: subscription.toJSON().keys.p256dh,
+          auth: subscription.toJSON().keys.auth,
+        },
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+
+      toast.success("Subscribe success");
+    } catch (e) {
+      setError(e);
+      if (e.errorCode === "ExistingSubscription") {
+        const registration = await navigator.serviceWorker.ready;
+        const convertedVapidKey = urlBase64ToUint8Array(PUBLIC_KEY);
+
+        const existingSubscription = await registration.pushManager.subscribe({
+          applicationServerKey: convertedVapidKey,
+          userVisibleOnly: true,
+        });
+
+        await axios.post(
+          BACKEND_URL + "/users/notifications/subscribe",
+          {
+            endpoint: existingSubscription.endpoint,
+            p256dh: existingSubscription.toJSON().keys.p256dh,
+            auth: existingSubscription.toJSON().keys.auth,
+          },
+          { headers: { Authorization: `Bearer ${accessToken}` } }
+        );
+
+        toast.success("Subscribe success");
+
+        console.log(
+          e,
+          existingSubscription.toJSON(),
+          existingSubscription.subscriptionId
+        );
+      } else {
+        console.warn(e);
+        toast.error("Details console");
+      }
+    } finally {
+      setLoadingSubscribe(false);
+    }
+  };
+
+  const onSubscribe = async () => {
+    setLoadingSubscribe(true);
+
     try {
       const subscription = await getSubscription();
       console.log(subscription.toJSON());
@@ -136,12 +191,14 @@ function App() {
   );
 
   useEffect(() => {
-    FingerprintJS.load()
-      .then((fp) => fp.get())
-      .then((result) => {
-        setSubscribeId(result.visitorId);
-        setPushId(result.visitorId);
-      });
+    // FingerprintJS.load()
+    //   .then((fp) => fp.get())
+    //   .then((result) => {
+    //     setSubscribeId(result.visitorId);
+    //     setPushId(result.visitorId);
+    //   });
+
+    onSubscribe();
   }, []);
 
   return (
