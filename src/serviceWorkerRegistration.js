e0d76d51f175abcacc99bd531ead9a3c37d0d9bc
stefko-tsonyovski-fntext <stefko.tsonyovski.ext@fntsoftware.com>
2024-02-22 17:44:36 +0200

    Commit

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -67,6 +67,8 @@ function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      toast.success("Register");
+
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
