0e65f733403d9bce6e3d1e09039ce6d2f3bf7e1e
PC <stefko_coniovski@abv.bg>
2024-02-22 22:43:04 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
 Notification.requestPermission((result) => {
   console.log(result);
   serviceWorkerRegistration.register();
